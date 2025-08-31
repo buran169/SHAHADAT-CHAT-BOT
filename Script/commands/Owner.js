5a53a23d49b11ed24ab0ac06e56178a5cd32fbc7
buran169 <mdburhanburhan1717@gmail.com>
2025-08-31 20:37:17 +0600

    Update Owner.js

--- Script/commands/Owner.js
+++ Script/commands/Owner.js
@@ -2,7 +2,7 @@ module.exports.config = {
  name: "owner",
  version: "1.0.1",
  hasPermssion: 0,
- credits: "𝐒𝐡𝐚𝐡𝐚𝐝𝐚𝐭 𝐈𝐬𝐥𝐚𝐦",
+ credits: "VIPER BURHAN,
  description: "Owner information command with styled box",
  commandCategory: "Information",
  usages: "",
@@ -14,24 +14,24 @@ module.exports.run = async ({ api, event }) => {
 `╔═════════════════════╗
 ║ 𝗢𝗪𝗡𝗘𝗥 𝗜𝗡𝗙𝗢 
 ╠═════════════════════╣
-║ 👤 𝗡𝗮𝗺𝗲 : 𝐒𝐇𝐀𝐇𝐀𝐃𝐀𝐓
-║ 🧸 𝗡𝗶𝗰𝗸 𝗡𝗮𝗺𝗲 : 𝗦𝗔𝗛𝗨
-║ 🎂 𝗔𝗴𝗲 : 𝟭𝟴+
+║ 👤 𝗡𝗮𝗺𝗲 : VIPER BURHAN
+║ 🧸 𝗡𝗶𝗰𝗸 𝗡𝗮𝗺𝗲 : BURHAN
+║ 🎂 𝗔𝗴𝗲 : 16+
 ║ 💘 𝗥𝗲𝗹𝗮𝘁𝗶𝗼𝗻 : 𝗦𝗶𝗻𝗴𝗹𝗲
 ║ 🎓 𝗣𝗿𝗼𝗳𝗲𝘀𝘀𝗶𝗼𝗻 : 𝗦𝘁𝘂𝗱𝗲𝗻𝘁
-║ 📚 𝗘𝗱𝘂𝗰𝗮𝘁𝗶𝗼𝗻 : 𝗛𝗦𝗖
-║ 🏡 𝗔𝗱𝗱𝗿𝗲𝘀𝘀 : 𝗞𝗵𝗮𝗴𝗿𝗮𝗰𝗵𝗮𝗿𝗶
+║ 📚 𝗘𝗱𝘂𝗰𝗮𝘁𝗶𝗼𝗻 : JSC
+║ 🏡 𝗔𝗱𝗱𝗿𝗲𝘀𝘀 : RANGPUR
 ╠═════════════════════╣
 ║ 𝗖𝗢𝗡𝗧𝗔𝗖𝗧 𝗟𝗜𝗡𝗞𝗦 
 ╠═════════════════════╣
 ║ 📘 𝗙𝗮𝗰𝗲𝗯𝗼𝗼𝗸 : 
-║ https://fb.com/100001039692046
+║ https://www.facebook.com/bullavaii01
 ║ 💬 𝗠𝗲𝘀𝘀𝗲𝗻𝗴𝗲𝗿 : 
-║ m.me/100001039692046
+║ NULL
 ║ 📞 𝗪𝗵𝗮𝘁𝘀𝗔𝗽𝗽 : 
-║ https://wa.me/01882333052
+║ NULL
 ║ ✈️ 𝗧𝗲𝗹𝗲𝗴𝗿𝗮𝗺 : 
-║ https://t.me/yoursahu
+║ https://t.me/burhan2244
 ╚═════════════════════╝`;
 
  return api.sendMessage(ownerInfo, event.threadID, event.messageID);
