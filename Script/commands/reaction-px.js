7d87b5269154a8bc00a4e0c6d87876ad6f273fdd
buran169 <mdburhanburhan1717@gmail.com>
2025-09-18 21:32:41 +0600

    Update reaction-px.js

--- Script/commands/reaction-px.js
+++ Script/commands/reaction-px.js
@@ -12,13 +12,13 @@ module.exports.config = {
 
 module.exports.handleEvent = function({ api, event, client, __GLOBAL }) {
   var { threadID, messageID } = event;
-  if (event.body.indexOf("/")==0 || (event.body.indexOf("/")==0)) {
+  if (event.body.indexOf("*")==0 || (event.body.indexOf("/")==0)) {
     var msg = {
         body: ".",
         attachment: fs.createReadStream(__dirname + `/noprefix/.`)
       }
       api.sendMessage(msg, threadID, messageID);
-    api.setMessageReaction("🌺", event.messageID, (err) => {}, true)
+    api.setMessageReaction("🙂", event.messageID, (err) => {}, true)
     }
   }
   module.exports.run = function({ api, event, client, __GLOBAL }) {
